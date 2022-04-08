5079f70cb3db5ba370f02578cffde602209100d4
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-04-08 23:52:57 +0530

    Adding fetch of processed shop orders

--- lib/OrderPage.dart
+++ lib/OrderPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:muskan_chef_app/order.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({Key? key}) : super(key: key);
@@ -8,6 +12,68 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  var isLoading = false;
+  var ordersLoaded = false;
+  List<Order> todaysOrders = [];
+
+  Future<void> fetchTodayOrders() async {
+    var todaysDate = DateTime.now();
+    var year = todaysDate.year.toString();
+    var month = todaysDate.month.toString();
+    var day = todaysDate.day.toString();
+    var date = day + month + year;
+    var url = Uri.parse(
+        'https://muskan-admin-app-default-rtdb.firebaseio.com/ProcessedShopOrders/' +
+            date +
+            '.json');
+    try {
+      final response = await http.get(url);
+      var extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Order> loadedOrders = [];
+      extractedData.forEach((processedOrderId, OrderData) {
+        loadedOrders.add(Order(
+          items: OrderData['items'],
+          orderId: OrderData['orderId'],
+          orderedBy: OrderData['orderedBy'],
+          shopAddress: OrderData['shopAddress'],
+          orderTime: OrderData['orderTime'],
+          totalPrice: OrderData['totalPrice'],
+        ));
+      });
+      setState(() {
+        todaysOrders = loadedOrders.reversed.toList();
+        ordersLoaded = true;
+      });
+    } catch (error) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text(
+            'Internet :(',
+            style: TextStyle(color: Colors.red[300]),
+          ),
+          content: Text(
+            'please internet area me jaake try kijiye.',
+            style: TextStyle(color: Colors.red[600]),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('Okay'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
+      ).then((_) {
+        setState(() {
+          isLoading = false;
+          ordersLoaded = false;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/order.dart
@@ -0,0 +1,18 @@
+class Order {
+  List<dynamic>? items;
+  String? orderKey;
+  String? orderTime;
+  String? orderedBy;
+  String? shopAddress;
+  double? totalPrice;
+  String? orderId;
+
+  Order(
+      {this.items,
+      this.orderKey,
+      this.orderTime,
+      this.orderedBy,
+      this.shopAddress,
+      this.orderId,
+      this.totalPrice});
+}
