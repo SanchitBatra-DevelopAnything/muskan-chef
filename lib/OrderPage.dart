959299fc8288199a1704a6d354478cfbbabf36c9
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-04-15 00:23:35 +0530

    Makimg yetToPrepare equal 0 on done button click

--- lib/OrderPage.dart
+++ lib/OrderPage.dart
@@ -49,7 +49,7 @@ class _OrderPageState extends State<OrderPage> {
         extractedData.forEach((processedOrderId, OrderData) {
           loadedOrders.add(Order(
             items: OrderData['items'],
-            orderId: OrderData['orderId'],
+            orderId: processedOrderId,
             orderedBy: OrderData['orderedBy'],
             shopAddress: OrderData['shopAddress'],
             orderTime: OrderData['orderTime'],
@@ -164,6 +164,57 @@ class _OrderPageState extends State<OrderPage> {
     return kaamKiList;
   }
 
+  Future<void> refreshOrders() async {
+    return fetchTodayOrders().then((_) {
+      formBiforcatedItemsList().then((itemlist) {
+        setState(() {
+          biforcatedItemsList = itemlist;
+          isLoading = false;
+        });
+      });
+    });
+  }
+
+  Future<void> preparedItems() async {
+    setState(() {
+      isLoading = true;
+    });
+    var todaysDate = DateTime.now();
+    var year = todaysDate.year.toString();
+    var month = todaysDate.month.toString();
+    var day = todaysDate.day.toString();
+    var date = day + month + year;
+    for (var i = 0; i < todaysOrders.length; i++) {
+      Order currentOrder = todaysOrders[i];
+      List? items = currentOrder.items;
+      String orderKey = currentOrder.orderId.toString();
+      for (var j = 0; j < items!.length; j++) {
+        if (items[j]["CategoryName"].toString().toLowerCase() ==
+            managedCategory.toString().toLowerCase()) {
+          print(managedCategory + " is being managed");
+          //update here and then send to backend , only update the keys to be updated
+          items[j] = {'yetToPrepare': 0, 'status': "PREPARED"};
+          var url = Uri.parse(
+              'https://muskan-admin-app-default-rtdb.firebaseio.com/ProcessedShopOrders/' +
+                  date +
+                  "/" +
+                  orderKey +
+                  "/items/$j.json");
+
+          await http.patch(url, body: json.encode(items[j]));
+        }
+      }
+      fetchTodayOrders().then((_) {
+        formBiforcatedItemsList().then((itemlist) {
+          setState(() {
+            biforcatedItemsList = itemlist;
+            isLoading = false;
+          });
+        });
+      });
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -190,7 +241,7 @@ class _OrderPageState extends State<OrderPage> {
           actions: [
             IconButton(
               icon: Icon(Icons.done_all),
-              onPressed: () {},
+              onPressed: preparedItems,
             ),
             IconButton(
                 onPressed: () {
@@ -200,13 +251,18 @@ class _OrderPageState extends State<OrderPage> {
           ],
         ),
         body: isLoading
-            ? Center(child: CircularProgressIndicator())
+            ? RefreshIndicator(
+                onRefresh: refreshOrders,
+                child: Center(child: CircularProgressIndicator()))
             : (ordersLoaded && zeroOrders)
-                ? Center(
-                    child: Text(
-                      'No orders',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ? RefreshIndicator(
+                    onRefresh: refreshOrders,
+                    child: Center(
+                      child: Text(
+                        'No orders',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
                     ),
                   )
                 : (ordersLoaded && noManagedItems)
@@ -216,24 +272,27 @@ class _OrderPageState extends State<OrderPage> {
                           style: TextStyle(fontSize: 20),
                         ),
                       )
-                    : ListView.builder(
-                        itemCount: biforcatedItemsList.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(
-                              biforcatedItemsList[index]
-                                  .item
-                                  .toString()
-                                  .toUpperCase(),
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            trailing: Text(
-                              biforcatedItemsList[index]
-                                  .yetToPrepare
-                                  .toString(),
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          );
-                        }));
+                    : RefreshIndicator(
+                        onRefresh: refreshOrders,
+                        child: ListView.builder(
+                            itemCount: biforcatedItemsList.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(
+                                  biforcatedItemsList[index]
+                                      .item
+                                      .toString()
+                                      .toUpperCase(),
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                trailing: Text(
+                                  biforcatedItemsList[index]
+                                      .yetToPrepare
+                                      .toString(),
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              );
+                            }),
+                      ));
   }
 }
