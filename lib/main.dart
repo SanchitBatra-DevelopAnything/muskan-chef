9853c7b6d4123238519bbec3981b28205163cbba
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-04-17 01:19:04 +0530

    Setting up push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:muskan_chef_app/OrderPage.dart';
 import 'package:muskan_chef_app/SplashScreen.dart';
 import 'LoginPage.dart';
+import 'notificationservice/local_notification_service.dart';
 
-void main() {
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  LocalNotificationService.initialize();
   runApp(MyApp());
 }
 
